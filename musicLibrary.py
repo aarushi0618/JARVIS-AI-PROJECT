a6e522d8aca9e7a59910681fade3fe6205a9e6dc
aarushi0618 <aarushi18096@gmail.com>
2026-05-03 21:18:44 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+   "stealth":"https://www.youtube.com/watch?v=f_9NLcyAblY",
+   "march":"https://www.youtube.com/watch?v=ph09xSEvw7w&list=RDph09xSEvw7w&start_radio=1",
+   "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1"
+
+}
+# key value pairs requires dictionary
